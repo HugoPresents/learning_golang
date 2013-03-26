45378a2555f21ec782624817b6534d4207a4aa28
Rabbit_52 <rabbitzhang52@gmail.com>
2013-03-26 18:21:32 +0800

    屁事没做~

---
+++ q15.go
@@ -0,0 +1,17 @@
+/*
+1. 编写一个函数返回另一个函数，返回的函数的作用是对一个整数 +2。
+函数的名称叫做 plusTwo。然后可以像下面这样使用：
+p := plusTwo()
+fmt.Printf("%v\n", p(2))
+应该打印 4。参阅第 36 页的 “回调” 小节了解更多相关信息。
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+}
+
+func 
