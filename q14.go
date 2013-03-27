7684631be0b92002dfa6eb7b3f5b086836c4b4bc
Rabbit_52 <rabbitzhang52@gmail.com>
2013-03-27 18:20:49 +0800

    start http

--- q14.go
+++ q14.go
@@ -7,9 +7,10 @@ package main
 import "fmt"
 
 func main() {
-    slice := []int{1, 3, 52, 45, 33, 12, 32, 22, 11, 5411, 343215, 3213, 24132, 21, 22, 14, 21}
+    slice := []int{243, 3, 52, 45, 33, 12, 32, 22, 11, 5411, 343215, 3213, 24132, 21, 22, 14, 21}
     fmt.Printf("%v before sort\n", slice)
-    sort(slice)
+    //sort(slice)
+    quick_sort(0, len(slice)-1, slice)
     fmt.Printf("%v after sort\n", slice)
 }
 
@@ -23,20 +24,24 @@ func sort(slice []int) {
     }
 }
 // 待完成~碎觉先。。。
-func fast_sort(slice []int) {
-    key := slice[0]
-    i, j := 0, len(slice) - 1
-    j_find := false
-    i_find := true
-    for j = i; j-- {
-        if slice[j] > slice[i] {
-            slice[j], slice[i] = slice[i], slice[j]
-            j_find = true
-            break
+func quick_sort(start, end int, slice []int) {
+    fmt.Printf("start is %d, end is %d\n", start, end)
+    if start == end {
+        return
+    }
+    key := slice[start]
+    mark := start
+    for i := start; i <= end; i++ {
+        fmt.Printf("key is %d, i is %d, value is %d\n", key, i, slice[i])
+        if slice[i] < key {
+            fmt.Printf("switch!\n")
+            slice[mark], slice[i] = slice[i], slice[mark]
+            mark = i
         }
     }
-    if()
-    for i = j; i ++ {
-        if slice[i] < slice
+    if mark + 1 > end {
+        return
     }
+    //quick_sort(start, mark, slice)
+    //quick_sort(mark + 1, end, slice)
 }
