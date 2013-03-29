673ec2ae42cdb97f44bc8a0ebc170a9b3c22f509
Rabbit_52 <rabbitzhang52@gmail.com>
2013-03-30 03:47:43 +0800

    time package

--- http.go
+++ http.go
@@ -2,12 +2,13 @@ package main
 
 import (
     "fmt"
+    "log"
     "net/http"
     "strings"
-    "log"
+    "time"
 )
 
-func hellow(w http.ResponseWriter, r *http.Request) {
+func hello(w http.ResponseWriter, r *http.Request) {
     r.ParseForm()
     fmt.Println(r.Form)
     fmt.Println("path", r.URL.Path)
@@ -21,9 +22,17 @@ func hellow(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    http.HandleFunc("/", hellow)
+    http.HandleFunc("/", hello)
+    http.HandleFunc("/time", my_time)
     err := http.ListenAndServe(":8888", nil)
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
     }
+}
+
+func my_time(w http.ResponseWriter, r *http.Request) {
+    t := time.Now()
+    fmt.Printf("Go launched at %s\n", t.Local())
+    fmt.Fprintf(w, "Date UTC:%s<br>", t.Local())
+    fmt.Fprintf(w, "Time Unix%d<br>", t.Unix())
 }
