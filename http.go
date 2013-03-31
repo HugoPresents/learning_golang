192eb5fc65cb3e814d05230a2c5f7dff837933b9
Rabbit_52 <rabbitzhang52@gmail.com>
2013-03-31 19:09:06 +0800

    templates & layout

--- http.go
+++ http.go
@@ -25,6 +25,9 @@ func hello(w http.ResponseWriter, r *http.Request) {
 func main() {
     http.HandleFunc("/", hello)
     http.HandleFunc("/time", my_time)
+    http.HandleFunc("/xss", xss)
+    http.HandleFunc("/templates", templates)
+    http.HandleFunc("/layout", layout)
     err := http.ListenAndServe(":8888", nil)
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
@@ -39,6 +42,23 @@ func my_time(w http.ResponseWriter, r *http.Request) {
 }
 
 func xss(w http.ResponseWriter, r *http.Request) {
-    t, err := template.New("foo").Parse(`{{define "T"}}Hello, {{.}}!{{end}}`)
-    err = t.ExecuteTemplate(out, "T", "<script>alert('you have been pwned')</script>")
+    t, _ := template.New("foo").Parse(`{{define "T"}}Hello, {{.}}!{{end}}`)
+    t.ExecuteTemplate(w, "T", "<script>alert('you have been pwned')</script>")
+}
+
+func layout(w http.ResponseWriter, r *http.Request) {
+    t, _ := template.ParseFiles("templates/layout.html", "templates/index.html")
+    t.ExecuteTemplate(w, "layout", nil)
+    t.Execute(w, nil)
+}
+
+func templates(w http.ResponseWriter, r *http.Request) {
+    s1, _ := template.ParseFiles("header.tmpl", "content.tmpl", "footer.tmpl")
+    s1.ExecuteTemplate(w, "header", nil)
+    fmt.Printf("header\n")
+    s1.ExecuteTemplate(w, "content", nil)
+    fmt.Printf("content\n")
+    s1.ExecuteTemplate(w, "footer", nil)
+    fmt.Printf("footer\n")
+    s1.Execute(w, nil)
 }
